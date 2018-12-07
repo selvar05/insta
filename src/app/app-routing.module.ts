7bdd3043ab1cf10f79f0f3d282178c7390dc70ca
selvar05 <selva-kumar.a.r@capgemini.com>
2018-12-07 16:39:34 +0530

    basic settings enabled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { MypostsComponent } from './myposts/myposts.component';
+import { FollowingComponent } from './following/following.component';
+import { PostsComponent } from './posts/posts.component';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'posts',component:PostsComponent},
+  {path:'following',component:FollowingComponent},
+  {path:'favourites',component:FavouritesComponent},
+  {path:'myposts',component:MypostsComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { MypostsComponent } from './myposts/myposts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PostsComponent,
+    FollowingComponent,
+    FavouritesComponent,
+    MypostsComponent
   ],
   imports: [
     BrowserModule,
